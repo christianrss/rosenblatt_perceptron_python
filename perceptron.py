d05de06f292657fddfbbb6cbac348c24fe7920d6
Christian <christianparadix@gmail.com>
2024-07-02 12:30:23 -0300

    Implementações Treinamento

--- perceptron.py
+++ perceptron.py
@@ -4,16 +4,20 @@ import re
 
 def main() -> int:
     bias = -1
-    
-    cpf_fake = '911.092.510-41'
     # Entradas (11)
-    Xi = re.split('[. -]', cpf_fake)
+    Xi = '91109251041'
     # Pesos (Um peso para cada numero do CPF). Obtido no treinamento
     Wi = [None] * 11
-    Wi[0] = -1; Wi[1] = 0; Wi[2] = 0; Wi[3] = 0; Wi[4] = 0; Wi[5] = 0
-    Wi[6] = 0; Wi[7] = 0; Wi[8] = 0; Wi[9] = 0; Wi[10] = 0
+    W0 = -1
+    Wi[0] = 0; Wi[1] = 0; Wi[2] = 0; Wi[3] = 0; Wi[4] = 0
+    Wi[5] = 0; Wi[6] = 0; Wi[7] = 0; Wi[8] = 0; Wi[9] = 0; Wi[10] = 0;
 
-    print(Wi)
+    saida = 0
+    for i in range(0, len(Xi)):
+        saida += int(Xi[i]) * Wi[i]
+    saida += W0 + bias
+
+    print(saida)
 
 if __name__ == '__main__':
     sys.exit(main())
---
+++ train.py
@@ -0,0 +1,16 @@
+import sys
+import typing
+import re
+
+def main() -> int:
+    bias = -1
+    taxa_aprendizado = 0.01
+    passo = 1
+
+    Wi = [None] * 11
+    W0 = -1
+    Wi[0] = 1; Wi[1] = 1; Wi[2] = 1; Wi[3] = 1; Wi[4] = 1
+    Wi[5] = 1; Wi[6] = 1; Wi[7] = 1; Wi[8] = 1; Wi[9] = 1; Wi[10] = 1;
+
+if __name__ == '__main__':
+    sys.exit(main())
